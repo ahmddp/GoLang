7855052781b42973c05cafc68b577f2f64ac462e
adp <ahmddp@gmail.com>
2019-01-03 15:33:43 +0300

    minor change for testing purpose

--- SimpleWebApp/main.go
+++ SimpleWebApp/main.go
@@ -6,11 +6,10 @@ import (
 )
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+	fmt.Fprintf(w, "Hello World !")
 }
 
 func main() {
 	http.HandleFunc("/", helloWorld)
 	http.ListenAndServe(":8080", nil)
 }
-
